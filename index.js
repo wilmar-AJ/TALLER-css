894f22f73ee4bf4c771ac92032b5705195ea4be2
wilmarAj <wilmarjimenez231@gmail.com>
2023-12-02 22:22:26 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const hamburguer = document.querySelector("#hamburguer");
+const closed = document.querySelector("#close");
+const menu = document.querySelector("#menu");
+
+hamburguer.addEventListener('click',()=>{
+    menu.style.display='flex';
+    closed.style.display='block';
+});
+closed.addEventListener('click',()=>{
+    menu.style.display='none';
+    closed.style.display='none';
+});
